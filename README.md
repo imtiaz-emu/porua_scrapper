8c572f374e4120bcda25704402d024c3cbe9202c
sirajus Salayhin <salayhin@gmail.com>
2017-01-07 15:11:26 +0600

    -  Author scrapy url scraping done - New data folder added for store data

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # Porua scrapper
+Scrap list of books, categories, authors from different sites
 
 ## Project Setup
 1. Clone https://github.com/imtiaz-emu/porua_scrapper.git
-2. Go to project directory and install dependencies
+
+2. Create a seperate `config.py` file. Where, you'll put the `SITE_URL`
+
+3. Go to project directory and install dependencies
  Command: `pip install -r requirements.txt`
 
-Scrap list of books, categories, authors from different sites 
+4. To run Category spider go to project folder and run on terminal:
+    `scrapy crawl categories -o data/categories.json`
+    This will create a **categories.json** file which contains the scrap data.
 
-To run a spider go to project folder and run on terminal:
+5. To run Author spider go to project folder and run on terminal 
+    `scrapy crawl authors -o data/authors.json` 
+    This will create a **author.json** file which contains the scrap data.
 
-`scrapy crawl categories -o categories.json`
 
-This will create a **categories.json** file which contains the scrap data.
 
-Create a seperate `config.py` file. Where, you'll put the `SITE_URL`
+
+
+
+
+
--- porua_scrapper/spiders/authors_spider.py
+++ porua_scrapper/spiders/authors_spider.py
@@ -9,35 +9,22 @@ class AuthorsSpider(scrapy.Spider):
     site_link = SITE_URL + '/book/authors'
     start_urls = [site_link]
 
-    # for I in range(1,847):
-    #     url = SITE_URL + "/book/authors?page=" + str(I)
-    #     start_urls.append(url)
-
     def parse(self, response):
-        self.author_link_list = []
-        self.parse_author_urls(response)
-        pdb.set_trace()
-        #pdb.set_trace()
-        # for author in response.css('ul.authorList li'):
-        #     file_path = open('author_urls.txt', 'a')
-        #     file_path.write(SITE_URL + author.css('a::attr(href)').extract_first() + '\n')
-        #     file_path.close()
-
-    def parse_author_urls(self, response):
         hxs = HtmlXPathSelector(response)
 
         authors = hxs.select("//section[@id='authorList']/div[@class='container']/ul//li")
 
-        #author_link_list = []
         for author in authors:
-            name = author.select("a/h2/text()").extract_first()
-            link = author.select("a/@href").extract_first()
-            # file_path = open('author_urls.txt', 'a')
+            author_obj = Author()
+            author_obj['name'] = author.select("a/h2/text()").extract_first()
+            author_obj['url'] = SITE_URL + author.select("a/@href").extract_first()
+            yield author_obj
+
+            # file_path = open('author_urls.csv', 'a')
             # file_path.write(SITE_URL + link + '\n')
             # file_path.close()
-            self.author_link_list.append({'name': name, 'link': SITE_URL + link})
 
         next_page = response.xpath("//div[@class='pagination']//a[position() = (last())]/@href").extract_first()
         if next_page is not None:
             next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse_author_urls)
+            yield scrapy.Request(next_page, callback=self.parse)
